77e824df9ee0934a8cfc690ff8c8361ad065026c
xxxxxmg <12364366+mht1234@user.noreply.gitee.com>
2023-06-03 16:54:22 +0800

    利用多线程创建文件并写入数据

---
+++ goroutine/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go test(i, &wg)
+	}
+	wg.Wait()
+}
+
+func test(id int, wg *sync.WaitGroup) {
+	name := fmt.Sprintf("%d.log", id)
+	fp, err := os.Create("./" + name)
+	if err != nil {
+		fmt.Println("Creat file err", err)
+	}
+	defer fp.Close()
+	defer wg.Done()
+	for i := id * 10; i < (id*10)+10; i++ {
+
+		if i == (id*10)+9 {
+			str := fmt.Sprintf("%d", i)
+			fp.WriteString(str)
+			return
+		}
+		str := fmt.Sprintf("%d\n", i)
+		fp.WriteString(str)
+	}
+}
